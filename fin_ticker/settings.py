2436248e43f200dd413285aee16f6b756da3e5ba
jk185256 <jk185256@ncr.com>
2022-11-26 18:00:53 -0600

    edit_settings

--- fin_ticker/settings.py
+++ fin_ticker/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'place-key-here'
 
-API_ACCESS_KEY = 'key here'
+API_ACCESS_KEY = 'place key here'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
